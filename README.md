70f87d7d09736f669ce6dbcdce1e6f7e12fa1aae
David Jozwik <david@djoz.us>
2023-05-25 22:18:32 -0500

    Finalize project

--- README.md
+++ README.md
@@ -1,11 +1,34 @@
 # LocalStack & Terraform REST API Test
 
-This is a simple guide on how to set up LocalStack and Terraform. This example will create an AWS Lambda function and connect it to an API Gateway that we can query locally.
+This is a simple guide on how to: set up LocalStack and Terraform. This example will create an AWS Lambda function and connect it to an API Gateway that we can query locally.
 
 # Steps
 1. Install Docker 
-   - Localstack runs as a Docker container, so we need to install Docker first. Here's a handy GUI client (Docker Desktop) from [here](https://www.docker.com/products/docker-desktop).
-2. Install Python and PIP
+   - Localstack runs as a Docker container, so we need to install Docker first. Here's a handy GUI client: [(Docker Desktop)](https://www.docker.com/products/docker-desktop).
+2. Install Python and pip
    - Localstack is installed using PIP, so we need to install Python and PIP first. You can download Python from [here](https://www.python.org/downloads/). PIP is included in Python 3.4 and above. But just in case here is PIP (https://pypi.org/project/pip/) 
-3. Install LocalStack
-   - 
+3. Install Terraform
+   - Install the CLI here: [Terraform CLI](https://developer.hashicorp.com/terraform/tutorials/aws-get-started/install-cli)
+4. Install Localstack and the Terraform provider with pip
+
+   ```shell
+   python -m pip install localstack
+   pip install terraform-local
+   ```
+5. Initialize Localstack Terraform provider and apply resources
+   - Make note of the API Gateway ID that is output in your terminal after running `tflocal apply`, you'll need it to query your API Gateway endpoint
+   Example: 
+   ```
+      aws_api_gateway_rest_api.api: Creation complete after 1s [id=1ltvwqpuju]
+   ````
+   - This will initialize the Terraform provider, build and package your lambda, and apply the resources defined in the `main.tf` file
+   - NOTE: The tflocal commands are functionally identical to the terraform commands, but they will automatically configure the AWS provider to use localstack
+   ```shell
+   tflocal init
+   tflocal apply
+   ```
+6. Query your API Gateway endpoint!
+   - Assemble an HTTP request using the API Gateway ID you noted earlier
+   - Syntax: `http://<API Gateway ID>.execute-api.localhost.localstack.cloud:4566/<stageId>/<path>`
+   - Example: `http://1ltvwqpuju.execute-api.localhost.localstack.cloud:4566/local/test`
+   - Example Response: `{"message":"Hello World!"}`
--- main.tf
+++ main.tf
@@ -1,3 +1,93 @@
-resource "aws_s3_bucket" "test-bucket" {
-  bucket = "my-bucket"
+// Set our AWS region
+provider "aws" {
+  region = "us-east-1"
+}
+
+// Build the lambda zip file
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "lambda/index.js"
+  output_path = "lambda/build/lambda.zip"
+}
+
+// Create the lambda function resource with our build ZIP file
+resource "aws_lambda_function" "lambda" {
+  function_name = "myTestLambda"
+
+  filename      = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  handler       = "index.handler"
+  runtime       = "nodejs18.x"
+
+  role          = aws_iam_role.lambda_exec.arn
+}
+
+// Create the execution IAM role for our lambda function
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+// Create the API Gateway
+resource "aws_api_gateway_rest_api" "api" {
+  name = "MyAPI"
+}
+
+// Add a path resource to our API gateway "/test"
+resource "aws_api_gateway_resource" "resource" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "test"
+}
+
+// Add a GET method to our "/test" resource
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+// Connect our lambda function to the GET "/test" resource
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.method.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda.invoke_arn
+}
+
+// Allow API Gateway to invoke our lambda function
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/GET/test"
+}
+
+// Create a deployment of our API Gateway named "local"
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on  = [aws_api_gateway_integration.integration]
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = "local"
+  description = "Deployed on ${timestamp()}"
 }
